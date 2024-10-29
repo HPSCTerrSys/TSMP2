6ac4c48f308a474a2cf4d564433958bd5390fc5c
kvrigor <kvrigor@users.noreply.github.com>
2024-10-29 16:27:34 +0100

    Various fixes for building ParFlow in Ubuntu
    NOTE: Not yet fully fixed!

--- cmake/BuildParFlow.cmake
+++ cmake/BuildParFlow.cmake
@@ -1,19 +1,26 @@
 find_package(NetCDF REQUIRED)
 find_package(Hypre REQUIRED)
 find_package(OpenMP REQUIRED)
+find_package(TCL REQUIRED)
 
 if(CMAKE_C_COMPILER_ID STREQUAL "GNU")
     # Flags were based from https://github.com/parflow/parflow/blob/77316043227b95215744e58fe9005d35145432ab/.github/workflows/linux.yml#L305
     set(PF_CFLAGS "${OpenMP_Fortran_FLAGS} -Wall -Werror -Wno-unused-result -Wno-unused-function")
+    set(PF_FFLAGS "-ffree-line-length-none -ffixed-line-length-none -ffree-form")
+    list(APPEND JSC_FLAGS -DPARFLOW_ENABLE_SLURM=OFF)
 elseif(CMAKE_C_COMPILER_ID STREQUAL "Intel" OR CMAKE_C_COMPILER_ID STREQUAL "IntelLLVM")
     set(PF_CFLAGS "${OpenMP_Fortran_FLAGS} -Wall -Werror")
-    set(PF_LDFLAGS "-lcudart -lcusparse -lcurand") #TODO: These linker flags are specific to JSC system!
+    set(PF_FFLAGS "-free")
+
+    #TODO: These flags are specific to JSC system. These flags must be machine-agnostic!
+    list(APPEND JSC_FLAGS -DPARFLOW_ENABLE_SLURM=ON)
+    list(APPEND JSC_FLAGS -DMPIEXEC_EXECUTABLE=${MPIEXEC_EXECUTABLE})
+    list(APPEND JSC_FLAGS -DMPIEXEC_NUMPROC_FLAG=${MPIEXEC_NUMPROC_FLAG})
+    list(APPEND JSC_FLAGS -DCMAKE_EXE_LINKER_FLAGS="-lcudart -lcusparse -lcurand")
 else()
   message(FATAL_ERROR "C compiler '${CMAKE_C_COMPILER_ID}' is not supported.")
 endif()
 
-
-set(PF_CFLAGS "${OpenMP_Fortran_FLAGS} ${OpenMP_Fortran_FLAGS}")
 if(DEFINED eCLM_SRC)
     list(APPEND PF_CLM_FLAGS -DPARFLOW_AMPS_LAYER=oas3
                              -DOAS3_ROOT=${OASIS_ROOT}
@@ -37,16 +44,15 @@ ExternalProject_Add(ParFlow
                 -DCMAKE_Fortran_COMPILER=${CMAKE_Fortran_COMPILER}
                 -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
                 -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
-                ${PF_CLM_FLAGS}
                 -DNETCDF_DIR=${NetCDF_ROOT}
                 -DPARFLOW_AMPS_SEQUENTIAL_IO=on
                 -DHYPRE_ROOT=${HYPRE_ROOT}
                 -DPARFLOW_ENABLE_TIMING=TRUE
-                -DPARFLOW_ENABLE_SLURM=TRUE
-                -DMPIEXEC_EXECUTABLE=${MPIEXEC_EXECUTABLE}
-                -DMPIEXEC_NUMPROC_FLAG=${MPIEXEC_NUMPROC_FLAG}
                 -DCMAKE_C_FLAGS=${PF_CFLAGS}
-                -DCMAKE_EXE_LINKER_FLAGS=${PF_LDFLAGS}
+                -DCMAKE_Fortran_FLAGS=${PF_FFLAGS}
+                -DTCL_TCLSH=${TCL_TCLSH}
+                ${PF_CLM_FLAGS}
+                ${JSC_FLAGS}
     DEPENDS     ${MODEL_DEPENDENCIES}
 )
 
