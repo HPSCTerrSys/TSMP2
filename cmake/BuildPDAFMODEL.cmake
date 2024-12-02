8e2f393546562650b81cc7feb1fe14312bbe7778
Johannes Keller <jo.keller@fz-juelich.de>
2024-12-02 10:23:40 +0100

    Comment out `USE_PDAF` flag for PDAFMODEL

--- cmake/BuildPDAFMODEL.cmake
+++ cmake/BuildPDAFMODEL.cmake
@@ -142,7 +142,7 @@ else()
   endif()
 endif()
 
-list(APPEND PDAF_DEFS "-DUSE_PDAF")
+# list(APPEND PDAF_DEFS "-DUSE_PDAF")
 if (CMAKE_BUILD_TYPE STREQUAL "DEBUG")
   # Debug run: Define `PDAF_DEBUG`
   list(APPEND PDAF_DEFS "-DPDAF_DEBUG")
